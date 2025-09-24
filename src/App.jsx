5c7c2ee2c0bf7a8d9be54c4a21c34bd1fbab0de6
PYPHT <panyaphat2.19@gmail.com>
2025-09-24 13:17:51 +0700

    Add motion animations and update Navbar component for enhanced UI transitions

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import { Navbar } from './components/Navbar'
 const App = () => {
   return (
     <div className='flex md:grid-rows-2'>
-      <div className='w-80 h-screen bg-gray-200 dark:bg-gray-800'>
+      <div className='w-80 h-screen bg-gray-200 dark:bg-gray-800 border-r-2 border-gray-100 dark:border-gray-500'>
         <Navbar />
       </div>
-      <div className='h-screen w-screen overflow-y-scroll'></div>
+      <div className='h-screen w-screen overflow-y-scroll bg-gray-200 dark:bg-gray-800'></div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import * as FaIcaons from "react-icons/fa6";
+import {fadeIn} from '../ultils/motion'
+import {motion} from 'motion/react'
 
 export const Navbar = () => {
   const [activeLink, setActiveLink] = useState('#admin')
@@ -12,19 +14,19 @@ export const Navbar = () => {
 
   return (
     <div className=''>
-      <div className='dark:bg-gray-600 bg-gray-300 border-b-2 border-gray-200 dark:border-gray-500'>
+      <motion.div variants={fadeIn('down', 0.2)} initial='hidden' whileInView='show' viewport={{ once: true }} className='dark:bg-gray-600 bg-gray-300 border-b-2 border-gray-200 dark:border-gray-500'>
         <h1 className='pl-2 pt-2 text-2xl font-bold dark:text-white text-blue-600'>SalesPro CRM</h1>
         <h2 className='pl-2 pb-2 font-semibold dark:text-white text-blue-600'>Management</h2>
-      </div>
-      <div>
+      </motion.div>
+      <motion.div variants={fadeIn('down', 0.4)} initial='hidden' whileInView='show' viewport={{ once: true }}>
         {
           menuItems.map((link, index) => (
-            <button id={index} type="button" className='cursor-pointer w-full hover:bg-gray-400 font-semibold text-gray-500 dark:text-gray-300 py-3 pl-5 dark:bg-gray-600 bg-gray-300'>
-              <a href={link.href} className='flex w-full h-full items-center group-hover:bg-gray-400'>{link.icon} <div className='pl-2'></div> {link.label}</a>
+            <button id={index} type="button" className={`cursor-pointer w-full group hover:bg-gray-400 font-semibold text-gray-500 dark:text-gray-300 py-3 pl-5 dark:bg-gray-600 bg-gray-300 ${activeLink == link.href ? 'bg-gray-400 dark:bg-gray-800' : ''}`}>
+              <a href={link.href} onClick={() => setActiveLink(link.href)} className='flex items-center hover:bg-gray-400'>{link.icon} <div className='pl-2'></div> {link.label}</a>
             </button>
           ))
         }
-      </div>
+      </motion.div>
     </div>
   )
 }
