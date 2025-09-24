8187d4cd8cf3ae4abd326a4d967223506455ba1e
PYPHT <panyaphat2.19@gmail.com>
2025-09-24 11:58:55 +0700

    Add react-icons dependency and enhance Navbar component with icons

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Navbar } from './components/Navbar'
 const App = () => {
   return (
     <div className='flex md:grid-rows-2'>
-      <div className='w-80 h-screen bg-gray-200'>
+      <div className='w-80 h-screen bg-gray-200 dark:bg-gray-800'>
         <Navbar />
       </div>
       <div className='h-screen w-screen overflow-y-scroll'></div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import * as FaIcaons from "react-icons/fa6";
 
 export const Navbar = () => {
+  const [activeLink, setActiveLink] = useState('#admin')
+  const menuItems = [
+    { href: "#admin", label: "Admins", icon: <FaIcaons.FaAddressCard /> },
+    { href: "#prod", label: "Products", icon: <FaIcaons.FaBoxesStacked /> },
+    { href: "#cont", label: "Contacts", icon: <FaIcaons.FaAddressBook /> },
+    { href: "#task", label: "Tasks", icon: <FaIcaons.FaListCheck /> },
+  ]
+
   return (
     <div className=''>
-        <div>
-            <h1 className='pl-2 pt-2 text-2xl font-bold'>SalesPro CRM</h1>
-            <h2 className='pl-2 pt-2 font-semibold'>Management</h2>
-        </div>
+      <div className='dark:bg-gray-600 bg-gray-300 border-b-2 border-gray-200 dark:border-gray-500'>
+        <h1 className='pl-2 pt-2 text-2xl font-bold dark:text-white text-blue-600'>SalesPro CRM</h1>
+        <h2 className='pl-2 pb-2 font-semibold dark:text-white text-blue-600'>Management</h2>
+      </div>
+      <div>
+        {
+          menuItems.map((link, index) => (
+            <button id={index} type="button" className='cursor-pointer w-full hover:bg-gray-400 font-semibold text-gray-500 dark:text-gray-300 py-3 pl-5 dark:bg-gray-600 bg-gray-300'>
+              <a href={link.href} className='flex w-full h-full items-center group-hover:bg-gray-400'>{link.icon} <div className='pl-2'></div> {link.label}</a>
+            </button>
+          ))
+        }
+      </div>
     </div>
   )
 }
