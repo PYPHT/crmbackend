5c6d8f0625ea1e3eef334e74b6011527faa2ca85
PYPHT <panyaphat2.19@gmail.com>
2025-09-26 12:16:19 +0700

    feat: add admin edit functionality and update image handling in server

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -2,14 +2,17 @@ import React, { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
 import { motion } from 'motion/react'
 import { fadeIn } from '../ultils/motion'
+import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
+import * as FaIcons from 'react-icons/fa6'
 
 const Admin = () => {
     const [admins, setAdmins] = useState([])
     const [departs, setDeparts] = useState([])
-    const [departsName, setDepartsName] = useState([])
     const [file, setFile] = useState(null)
     const [image, setImage] = useState("https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500");
+    const [imageEdit, setImageEdit] = useState("")
     const fileInputRef = useRef(null);
+    const [open, setOpen] = useState(false)
 
     const [username, setUserName] = useState("")
     const [password, setPassword] = useState("")
@@ -45,6 +48,26 @@ const Admin = () => {
         }
     }
 
+    const handleEditAdmin = async () => {
+        const formData = new FormData()
+        formData.append("username", username)
+        formData.append("password", password)
+        formData.append("fname", fname)
+        formData.append("lname", lname)
+        formData.append("email", email)
+        formData.append("phone", phone)
+        formData.append("status", "0")
+        formData.append("type", type)
+        formData.append("department", department)
+        formData.append("image", file)
+
+        const res = await axios.post("http://192.168.1.30:5001/insertadmins", formData)
+        if (res.status === 200) {
+            getAdmins()
+            window.location.reload()
+        }
+    }
+
     const getAdmins = async () => {
         try {
             const res = await axios.post("http://192.168.1.30:5001/getadmins")
@@ -58,15 +81,6 @@ const Admin = () => {
         try {
             const res = await axios.post("http://192.168.1.30:5001/getdepartment")
             setDeparts(res.data)
-        } catch (error) {
-            console.error(error)
-        }
-    }
-
-    const getDepartmentName = async (id = null) => {
-        try {
-            const res = await axios.post("http://192.168.1.30:5001/getdepartment")
-            setDepartsName(res.data)
         } catch (error) {
             console.error(error)
         }
@@ -86,6 +100,15 @@ const Admin = () => {
         if (file) {
             const imageUrl = URL.createObjectURL(file)
             setImage(imageUrl)
+            setFile(file)
+        }
+    };
+
+    const handleFileEdit = (e) => {
+        const file = e.target.files[0]
+        if (file) {
+            const imageUrl = URL.createObjectURL(file)
+            setImageEdit(imageUrl)
             setFile(file)
         }
     };
@@ -111,8 +134,8 @@ const Admin = () => {
                             </td>
                         </tr>
                         {
-                            admins.length > 0 ? admins.map((admin, index) => (
-                                <motion.tr variants={fadeIn('left', 0.4)} initial='hidden' whileInView='show' viewport={{ once: true }} key={index} className='dark:text-white text-gray-600 font-semibold border-b-1 dark:border-gray-500 border-gray-300'>
+                            admins.map((admin, index) => (
+                                <motion.tr variants={fadeIn('left', 0.4)} initial='hidden' whileInView='show' viewport={{ once: true }} key={index} className='dark:text-white text-gray-600 font-semibold border-b-1 dark:border-gray-500 border-gray-300 border-t-1'>
                                     <td className='text-center'>{index + 1}</td>
                                     <td className='py-2'>
                                         <div className='flex items-center'>
@@ -126,15 +149,84 @@ const Admin = () => {
                                             </div>
                                         </div>
                                     </td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
+                                    <td className='text-center'>{admin.departmentName}</td>
+                                    <td className='text-center'>{new Date(admin.createtime).toLocaleDateString("th-TH", {
+                                        year: "numeric",
+                                        month: "2-digit",
+                                        day: "2-digit",
+                                    })}<br />{new Date(admin.createtime).toLocaleTimeString("th-TH", {
+                                        hour: "2-digit",
+                                        minute: "2-digit",
+                                    })}
+                                    </td>
+                                    <td className='text-center'>{admin.status === 1 ? 'Active' : admin.status === 0 ? 'Offline' : 'Retired'}</td>
+                                    <td className='text-center'>
+                                        <div>
+                                            <button onClick={() => setOpen(true)} type="button" className='bg-orange-500/50 p-2 rounded-lg hover:bg-orange-600/50 cursor-pointer'>Edit</button>
+                                            <Dialog open={open} onClose={setOpen} className="relative z-20">
+                                                <DialogBackdrop
+                                                    transition
+                                                    className="fixed inset-0 bg-gray-900/90 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"
+                                                />
+
+                                                <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                                                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                                                        <DialogPanel
+                                                            transition
+                                                            className="relative transform overflow-hidden rounded-lg bg-gray-800 text-left shadow-xl outline -outline-offset-1 outline-white/10 transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95"
+                                                        >
+                                                            <div className="bg-gray-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                                                                <div className="sm:flex sm:items-start">
+                                                                    <div className="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-orange-500/10 sm:mx-0 sm:size-10">
+                                                                        <FaIcons.FaPenToSquare className='text-orange-600' />
+                                                                    </div>
+                                                                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                                                        <DialogTitle as="h3" className="text-base font-semibold text-white">
+                                                                            Edit admin account
+                                                                        </DialogTitle>
+                                                                        <div className="mt-2">
+                                                                            <div className='flex items-center'>
+                                                                                <img src={imageEdit === "" ? admin.imageUrl : imageEdit} alt={admin.username} className='w-[120px] h-[120px] rounded-full object-cover' />
+                                                                                <button type='button' onClick={handleButtonClick} className='bg-blue-600 hover:bg-blue-700 cursor-pointer h-max p-1 m-5 rounded-lg text-sm font-semibold text-white'>
+                                                                                    + Edit Image
+                                                                                    <input
+                                                                                        type="file"
+                                                                                        accept="image/png, image/jpeg"
+                                                                                        ref={fileInputRef}
+                                                                                        onChange={handleFileEdit}
+                                                                                        className="hidden"
+                                                                                    />
+                                                                                </button>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div className="bg-gray-700/25 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                                                <button
+                                                                    type="button"
+                                                                    onClick={() => setOpen(false)}
+                                                                    className="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white hover:bg-blue-700 sm:ml-3 sm:w-auto"
+                                                                >
+                                                                    Save
+                                                                </button>
+                                                                <button
+                                                                    type="button"
+                                                                    data-autofocus
+                                                                    onClick={() => setOpen(false)}
+                                                                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white inset-ring inset-ring-white/5 hover:bg-white/20 sm:mt-0 sm:w-auto"
+                                                                >
+                                                                    Cancel
+                                                                </button>
+                                                            </div>
+                                                        </DialogPanel>
+                                                    </div>
+                                                </div>
+                                            </Dialog>
+                                        </div>
+                                    </td>
                                 </motion.tr>
-                            )) :
-                                <tr>
-                                    <td colSpan={5} className='text-center py-5 text-lg font-semibold dark:text-white'>No Data</td>
-                                </tr>
+                            ))
                         }
                     </tbody>
                 </table>
--- src/connection/server.cjs
+++ src/connection/server.cjs
@@ -28,15 +28,15 @@ app.post("/insertadmins", upload.single("image"), async (req, res) => {
             .input("lname", sql.VarChar, lname)
             .input("email", sql.VarChar, email)
             .input("phone", sql.VarChar, phone)
-            .input("imageproflie", sql.VarBinary, req.file.buffer)
+            .input("imageprofile", sql.VarBinary, req.file.buffer)
             .input("status", sql.Int, status)
             .input("type", sql.Int, type)
             .input("depart", sql.Int, department)
             .input("createtime", sql.VarChar, createtime)
             .input("logintime", sql.VarChar, logintime)
             .query(`
-        INSERT INTO admins (username, password, fname, lname, email, phone, imageproflie, status, type, depart, createtime, logintime)
-        VALUES (@username, @password, @fname, @lname, @email, @phone, @imageproflie, @status, @type, @depart, @createtime, @logintime)
+        INSERT INTO admins (username, password, fname, lname, email, phone, imageprofile, status, type, depart, createtime, logintime)
+        VALUES (@username, @password, @fname, @lname, @email, @phone, @imageprofile, @status, @type, @depart, @createtime, @logintime)
       `);
 
         res.status(200).json({ success: true });
@@ -46,24 +46,64 @@ app.post("/insertadmins", upload.single("image"), async (req, res) => {
     }
 })
 
+app.post("/editadmins", upload.single("image"), async (req, res) => {
+    const { username, password, fname, lname, email, phone, status, type, department, id } = req.body;
+    try {
+        const pool = await sql.connect(dbConfig);
+
+        await pool.request()
+            .input("username", sql.VarChar, username)
+            .input("password", sql.VarChar, password)
+            .input("fname", sql.VarChar, fname)
+            .input("lname", sql.VarChar, lname)
+            .input("email", sql.VarChar, email)
+            .input("phone", sql.VarChar, phone)
+            .input("imageprofile", sql.VarBinary, req.file.buffer)
+            .input("status", sql.Int, status)
+            .input("type", sql.Int, type)
+            .input("depart", sql.Int, department)
+            .input("id", sql.Int, id)
+            .query(`
+        UPDATE admins
+        SET 
+            username = @username,
+            password = @password,
+            fname = @fname,
+            lname = @lname,
+            email = @email,
+            phone = @phone,
+            imageprofile = @imageprofile,
+            status = @status,
+            type = @type,
+            depart = @depart
+        WHERE id = @id
+    `)
+
+        res.status(200).json({ success: true })
+    } catch (error) {
+        console.error(err);
+        res.status(500).send("Upload error");
+    }
+})
+
 app.post("/getadmins", async (req, res) => {
     try {
         const pool = await sql.connect(dbConfig)
-
         const result = await pool.request()
-            .query(`SELECT *, CAST(imageproflie AS VARBINARY(MAX)) AS imageprofile_bin FROM admins`);
+            .query(`
+        SELECT a.*, d.department AS departmentName
+        FROM admins a
+        INNER JOIN department d ON a.depart = d.id
+      `)
 
-        const admins = result.recordset.map(admin => {
-            let imageBase64 = null;
-            if (admin.imageprofile_bin) {
-                imageBase64 = Buffer.from(admin.imageprofile_bin).toString('base64')
+        const admins = result.recordset.map(row => {
+            if (row.imageprofile && Buffer.isBuffer(row.imageprofile)) {
+                row.imageUrl = `data:image/jpeg;base64,${Buffer.from(row.imageprofile).toString("base64")}`
+            } else {
+                row.imageUrl = null
             }
-
-            return {
-                ...admin,
-                imageUrl: imageBase64 ? `data:image/jpeg;base64,${imageBase64}` : null
-            }
-        })
+            return row
+        });
 
         res.status(200).json(admins)
     } catch (error) {
@@ -72,21 +112,12 @@ app.post("/getadmins", async (req, res) => {
     }
 })
 
-
 app.post("/getdepartment", async (req, res) => {
-    const departID = req.body.id
     try {
         const pool = await sql.connect(dbConfig)
-        if (departID != null) {
-            const result = await pool.request()
-                .input("id", sql.Int, departID)
-                .query(`SELECT department FROM department WHERE id = @id `)
-            res.status(200).json(result.recordset)
-        } else {
-            const result = await pool.request()
-                .query(`SELECT * FROM department`)
-            res.status(200).json(result.recordset)
-        }
+        const result = await pool.request()
+            .query(`SELECT * FROM department`)
+        res.status(200).json(result.recordset)
     } catch (error) {
         console.error("DB Error:", error);
         res.status(500).json({ error: "Database query failed" });
